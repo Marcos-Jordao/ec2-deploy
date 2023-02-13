86d16c20d7bbcd611b0f9f7e01baa1a068629b96
Marcos Vinicius Pinto Jordao <45662689+Marcos-Jordao@users.noreply.github.com>
2023-02-13 12:26:24 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "s3" {}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.AWS_DEFAULT_REGION
+  assume_role {
+    role_arn = "arn:aws:iam::${var.ACCOUNT_ID}:role/Terraform_Assume"
+  }
+}
+
+module "my-ec2" {
+ source   = "./modules/ec2"
+ environment = var.environment
+ ec2_size = "t3a.micro"
+}
