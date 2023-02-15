9ddd49bf22658c920d42344d7f8dead4c530a92c
Marcos Vinicius Pinto Jordao <45662689+Marcos-Jordao@users.noreply.github.com>
2023-02-15 13:52:29 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
-output "region" {
-  description = "Deploy Region"
-  value       = data.aws_region.current.name
-}
+#output "region" {
+# description = "Deploy Region"
+#  value       = data.aws_region.current.name
+#}
 
-output "account_id" {
-  value = data.aws_caller_identity.current.account_id
-}
+#output "account_id" {
+#  value = data.aws_caller_identity.current.account_id
+#}
